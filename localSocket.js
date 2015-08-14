70f547d1a1dead5095ae9aeaf8bc3d2e76bc96be
Jan-Ivar Bruaroey <wingboy@gmail.com>
2015-08-14 00:53:54 -0400

    initial version

---
+++ localSocket.js
@@ -0,0 +1,27 @@
+function localSocket() {
+  localStorage.a = localStorage.b = JSON.stringify([]);
+  this.index = 0;
+  this.interval = setInterval(() => {
+    if (!this.in) {
+      if (!JSON.parse(localStorage.a).length) return;
+      this.in = "a"; this.out = "b";
+    }
+    var arr = JSON.parse(localStorage[this.in]);
+    if (arr.length <= this.index) return;
+    if (this.onmessage) this.onmessage(arr[this.index]);
+    this.index++;
+  }, 200);
+}
+localSocket.prototype = {
+  send: function(msg) {
+    if (!this.out) {
+      this.out = "a"; this.in = "b";
+    }
+    var arr = JSON.parse(localStorage[this.out]);
+    arr.push(msg);
+    localStorage[this.out] = JSON.stringify(arr);
+  },
+  close: function() {
+    clearInterval(this.interval);
+  }
+};
